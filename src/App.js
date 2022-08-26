58854df5d2e181f900977aece052bc2716dd21e0
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-08-26 20:48:12 -0300

    Escolha da palavra e categoria

--- src/App.js
+++ src/App.js
@@ -22,8 +22,44 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickeCategory, setPickedCategory] = useState("");
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    // pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    console.log(word);
+
+    return { word, category };
+
+  };
+
   // starts the secret word game
   const startGame = () => {
+   // pick word and pick category
+    const { word, category } = pickedWordAndCategory();
+
+    // create an array of letters
+    let wordLettters = word.split("");
+
+    wordLettters = wordLettters.map((l) => l.toLowerCase())
+
+    console.log(word, category);
+    console.log(wordLettters);
+
+    // fill states
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(letters);
+
     setGameStage(stages[1].name);
   };
 
