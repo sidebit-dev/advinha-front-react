151b1b40d8665547d55a136446f390e62efe2870
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-08-26 21:05:20 -0300

    Estruturando o componente Jogo

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,11 +2,32 @@ import './Game.css';
 
 const Game = ({verifyLetter}) => {
   return (
-    <div>
-        <h1>Game</h1>
-        <button onClick={verifyLetter}>Finalizar jogo</button>
+    <div className='game'>
+        <p className='points'>
+            <span>Pontuação: 000</span>
+        </p>
+        <h1>Advinhe a palavra:</h1>
+        <h3 className='tip'>
+            Dica sobre a palavra: <span>Dica...</span>
+        </h3>
+        <div className='wordContainer'>
+            <span className='letter'>A</span>
+            <span className='blankSquere'></span>
+        </div>
+        <div className='letterContainer'>
+            <p>Tente advinhar uma letra da palavra:</p>
+            <form>
+                <input type="text" name="letter" maxlength="1" requered />
+                <button>Jogar!</button>
+            </form>
+        </div>
+        <div className='wrongLettersContainer'>
+            <p>Letras já utilizadas:</p>
+            <span>a, </span>
+            <span>b, </span>
+        </div>
     </div>
-  )
-}
+  );
+};
 
 export default Game
