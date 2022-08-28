6c3614de81c0dac5eecf45d51c877dc173af10a7
Miguel Arcanjo <miguel.arcanjo@sidebit.dev>
2022-08-28 00:32:24 -0300

    Logica para fim de jogo

--- src/App.js
+++ src/App.js
@@ -18,6 +18,8 @@ const stages = [
   {id: 3, name: "end"}
 ];
 
+const guessesQty = 3;
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
@@ -28,7 +30,7 @@ function App() {
 
   const [guessedLetters, setGuessedLetters] = useState([]);
   const [wrongLetters, setWrongLetters] = useState([]);
-  const [guesses, setGuesses] = useState(3);
+  const [guesses, setGuesses] = useState(guessesQty);
   const [score, setScore] = useState(0);
 
   const pickedWordAndCategory = () => {
@@ -85,21 +87,40 @@ function App() {
       setGuessedLetters((actualGuessedLetters) => [
         ...actualGuessedLetters,
         normalizedLetter,
-      ])
+      ]);
     } else {
       setWrongLetters((actualWrongLetters) => [
         ...actualWrongLetters,
         normalizedLetter,
-      ])
+      ]);
+
+      setGuesses((actualGuesses) => actualGuesses -1);
     }
   };
 
 
-  console.log(guessedLetters);
-  console.log(wrongLetters);
+  // console.log(guessedLetters);
+  // console.log(wrongLetters);
+  const clearLetterStates = () => {
+    setGuessedLetters([]);
+    setWrongLetters([]);
+   }
+
+  useEffect(() => {
+
+    if(guesses <= 0) {
+      // reset all states
+      clearLetterStates();
+
+      setGameStage(stages[2].name);
+    }
+  }, [guesses])
 
   // Restart the game
   const retry = () => {
+    setScore(0);
+    setGuesses(guessesQty);
+
     setGameStage(stages[0].name);    
   };
 
